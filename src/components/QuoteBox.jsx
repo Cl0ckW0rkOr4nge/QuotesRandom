dfdb1984d77144fa297f495430da9e2c8931c0ad
Cl0ckW0rkOr4nge <112908757+Cl0ckW0rkOr4nge@users.noreply.github.com>
2022-09-20 16:42:19 -0500

    Add files via upload

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const QuoteBox = ({ randomQuote, randomColor, getRandomAll }) => {
+
+    console.log(randomColor)
+
+    const colorObj = {
+        color: randomColor
+    }
+
+    const backgroundObj = {
+        backgroundColor: randomColor
+    }
+
+    return (
+        <article style={colorObj} className='card'>
+            <p className='card_quote'>{randomQuote.quote}</p>
+            <h1 className='card_author'>{randomQuote.author}</h1>
+            <button
+            className='card_btn'
+            style={backgroundObj}
+            onClick={getRandomAll}
+            >Next quote &#62;</button>
+        </article>
+    )
+
+}
+
+export default QuoteBox
---
+++ src/utils/color.js
@@ -0,0 +1,3 @@
+const color = ['#9f0767', '#ebdf01', '#3fd37c', '#f08c81', '#ab8ca1', '#bf1716', '#a0e1ce']
+
+export default color
